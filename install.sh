c0a7323f7c761302f136b7ae9cecd0a693a7da27
YAbl0K0 <32619526+YAbl0K0@users.noreply.github.com>
2024-08-30 01:58:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ npm install bip39
 mkdir leap-wallet-generator
 cd leap-wallet-generator
 
-git clone https://github.com/VadimRM7/PUBLIC/wallet/leep_wallet25.git
+git clone https://github.com/VadimRM7/leep_wallet25.git
 cd leep_wallet25
 npm install
 
